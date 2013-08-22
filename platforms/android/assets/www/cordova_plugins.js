2799242296f893202f651122085f44aa0b4e07f0
Greg Koenig <gkoenig@fullsail.edu>
2013-08-21 22:43:35 -0400

    annnddd android … still doesnt work.

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -134,6 +134,41 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraConstants.js",
+        "id": "org.apache.cordova.core.camera.Camera",
+        "clobbers": [
+            "Camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverOptions.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverOptions",
+        "clobbers": [
+            "CameraPopoverOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/Camera.js",
+        "id": "org.apache.cordova.core.camera.camera",
+        "clobbers": [
+            "navigator.camera"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.camera/www/CameraPopoverHandle.js",
+        "id": "org.apache.cordova.core.camera.CameraPopoverHandle",
+        "clobbers": [
+            "CameraPopoverHandle"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.core.inappbrowser/www/InAppBrowser.js",
+        "id": "org.apache.cordova.core.inappbrowser.InAppBrowser",
+        "clobbers": [
+            "window.open"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.core.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.core.file.DirectoryEntry",
         "clobbers": [
